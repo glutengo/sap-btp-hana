7f37677135c308e5bdb2c9ce0eac66f84d16f160
glutengo <markus.glutting@desso.de>
2023-12-08 14:12:23 +0000

    Fiori

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -2,6 +2,23 @@ const cds = require('@sap/cds');
 module.exports = cds.service.impl(function () {
     const {Products} = this.entities();
 
+    this.on('get_supplier_info', async () => {
+        try {
+            const db = await cds.connect.to('db');
+            const dbclass = require('sap-hdbext-promisfied');
+            console.log(db.options.credentials);
+            let dbConn = new dbclass(await dbclass.createConnection(db.options.credentials));
+            const hdbext = require('@sap/hdbext');
+            const sp = await dbConn.loadProcedurePromisified(hdbext, null, 'get_supplier_info');
+            const output = await dbConn.callProcedurePromisified(sp, []);
+            console.log(output.results);
+            return output.results;
+        } catch (error) {
+            console.error(error);
+            return;
+        }
+    });
+
     this.after('each', Products, row => {
         console.log(`Read Product: ${row.ID}`);
     });
@@ -13,4 +30,6 @@ module.exports = cds.service.impl(function () {
             this.emit('prod_Change', header);    
         });
     });
+
+    
 });
